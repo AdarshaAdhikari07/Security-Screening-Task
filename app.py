0ac3f0597c7dba641e30ca7d73d17aaff639b615
Adarsha Adhikari <131242802+AdarshaAdhikari07@users.noreply.github.com>
2026-02-19 14:48:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if not st.session_state.consent_given:
         st.write("**Researcher:** Adarsha Adhikari | **Supervisor:** Dr Mark Eslaw ")
         st.write("**** ")
         st.markdown(f"""
-        **Purpose:** This research aims to compare the prototypes of Manual and Ai-Assisted for security screening in order to study "Autumation Bias"" .
+        **Purpose:** This research aims to compare the prototypes of Manual and Ai-Assisted modes for security screening in order to study "Automation Bias"" .
         **Procedure:** You are required to detect threat items by inspecting 10 bags, each in two different modes : Manual and Ai-Assisted.
         **Privacy:** No names, IP addresses, or Personal Identified Data  are recorded.
         **Data Submission:** After completing the inspection download the anonymous CSV file and email it to **adhika108@coventry.ac.uk**.
